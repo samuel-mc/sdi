cb7103a8a18179d42cc55e30d91d937cb76d81f6
Samuel Medina <smdoomed@gmail.com>
2025-01-03 21:35:48 -0600

    Update files

---
+++ public/js/index.js
@@ -0,0 +1,9 @@
+// Cargar navbar.html en el div con id "navbar"
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error al cargar la navbar:', error));
+});
