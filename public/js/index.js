e196ed9a8e3234a1f78b1f9c0ce8b75b8f5adc99
Samuel Medina <smdoomed@gmail.com>
2025-01-05 20:25:49 -0600

    se agrega el footer

--- public/js/index.js
+++ public/js/index.js
@@ -7,3 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
         })
         .catch(error => console.error('Error al cargar la navbar:', error));
 });
+
+// Cargar footer.html en el div con id "footer"
+document.addEventListener("DOMContentLoaded", function () {
+    fetch('footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.error('Error al cargar el footer:', error));
+});
